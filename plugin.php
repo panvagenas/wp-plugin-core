6949ddd202ae4cff0e49307908b52a3acb42c021
Panagiotis Vagenas <pan.vagenas@gmail.com>
2015-08-29 17:00:45 +0300

    Init

--- plugin.php
+++ plugin.php
@@ -20,24 +20,3 @@ if ( ! defined( 'WPINC' ) ) {
 require_once __DIR__ . '/vendor/autoload.php';
 
 $plugin = new \PanWPCore\Plugin( __FILE__, 'Pan WordPress Plugin Core', '0.0.1-dev', 'pan-core', 'pan.core' );
-
-$redux = new \PanWPCore\Redux( $plugin, [ ] );
-$redux->addSection( 'A section', 'a-section' );
-$redux->addField( 'a-section', [
-	'id'       => 'opt-checkbox',
-	'type'     => 'checkbox',
-	'title'    => __( 'Checkbox Option', 'redux-framework-demo' ),
-	'subtitle' => __( 'No validation can be done on this field type', 'redux-framework-demo' ),
-	'desc'     => __( 'This is the description field, again good for additional info.', 'redux-framework-demo' ),
-	'default'  => '1'// 1 = on | 0 = off
-] );
-
-$redux->addField( 'a-section', [
-	'id'       => 'opt-checkbox2',
-	'type'     => 'checkbox',
-	'title'    => __( 'Checkbox Option 2', 'redux-framework-demo' ),
-	'subtitle' => __( 'No validation can be done on this field type', 'redux-framework-demo' ),
-	'desc'     => __( 'This is the description field, again good for additional info.', 'redux-framework-demo' ),
-	'default'  => '1'// 1 = on | 0 = off
-] );
-
