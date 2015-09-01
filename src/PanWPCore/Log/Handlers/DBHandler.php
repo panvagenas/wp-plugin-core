098406b004956f1a09663b6097e947dbbafc3f64
Panagiotis Vagenas <pan.vagenas@gmail.com>
2015-09-01 08:31:10 +0300

    Implementing DBHandler for logging to DB

--- src/PanWPCore/Log/Handlers/DBHandler.php
+++ src/PanWPCore/Log/Handlers/DBHandler.php
@@ -11,12 +11,10 @@ namespace PanWPCore\Log\Handlers;
 
 use Monolog\Handler\AbstractProcessingHandler;
 use Monolog\Logger;
-use PanWPCore\Plugin;
 
 class DBHandler extends AbstractProcessingHandler {
-	private $statement;
 	private $logName;
-	private $logs = [ ];
+	private $logs = array();
 
 	public function __construct( $logName, $level = Logger::DEBUG, $bubble = true ) {
 		$this->logName = $logName;
