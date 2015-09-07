9d738eb83bae725e35753108f997258ffb842ed3
Panagiotis Vagenas <pan.vagenas@gmail.com>
2015-09-07 06:46:54 +0300

    Fixing the way scripts are enqueued

--- src/PanWPCore/Scripts.php
+++ src/PanWPCore/Scripts.php
@@ -12,7 +12,7 @@
 namespace PanWPCore;
 
 
-class Scripts extends Core{
+class Scripts extends Core {
 	/**
 	 * @param $file
 	 *
@@ -20,11 +20,11 @@ class Scripts extends Core{
 	 * @author Panagiotis Vagenas <pan.vagenas@gmail.com>
 	 * @since  TODO ${VERSION}
 	 */
-	public function findScriptFile($file){
-		$file = $this->Paths->removeDots($file);
+	public function findScriptFile( $file ) {
+		$file = $this->Paths->removeDots( $file );
 
 		foreach ( $this->Paths->whereScriptsMayReside as $dir ) {
-			if(file_exists($path = $dir . DIRECTORY_SEPARATOR . $file) && is_readable($path)){
+			if ( file_exists( $path = $dir . DIRECTORY_SEPARATOR . $file ) && is_readable( $path ) ) {
 				return $path;
 			}
 		}
@@ -39,11 +39,11 @@ class Scripts extends Core{
 	 * @author Panagiotis Vagenas <pan.vagenas@gmail.com>
 	 * @since  TODO ${VERSION}
 	 */
-	public function findStyleFile($file){
-		$file = $this->Paths->removeDots($file);
+	public function findStyleFile( $file ) {
+		$file = $this->Paths->removeDots( $file );
 
 		foreach ( $this->Paths->whereStylesMayReside as $dir ) {
-			if(file_exists($path = $dir . DIRECTORY_SEPARATOR . $file) && is_readable($path)){
+			if ( file_exists( $path = $dir . DIRECTORY_SEPARATOR . $file ) && is_readable( $path ) ) {
 				return $path;
 			}
 		}
@@ -53,7 +53,65 @@ class Scripts extends Core{
 
 	/**
 	 * @param            $handle
-	 * @param            $src
+	 * @param bool|false $src
+	 * @param array      $deps
+	 * @param bool|false $ver
+	 * @param string     $media
+	 * @param string     $hook
+	 *
+	 * @author Panagiotis Vagenas <pan.vagenas@gmail.com>
+	 * @since  TODO ${VERSION}
+	 */
+	public function enqueueStyle(
+		$handle,
+		$src = false,
+		$deps = array(),
+		$ver = false,
+		$media = 'all',
+		$hook = 'wp_enqueue_scripts'
+	) {
+		if ( ! wp_style_is( $handle, 'enqueued' ) ) {
+			if ( ! $ver ) {
+				$ver = $this->Plugin->getVersion();
+			}
+			add_action( $hook, function () use ( $handle, $src, $deps, $ver, $media ) {
+				wp_enqueue_style( $handle, $src, $deps, $ver, $media );
+			}, 20 );
+		}
+	}
+
+	/**
+	 * @param            $handle
+	 * @param bool|false $src
+	 * @param array      $deps
+	 * @param bool|false $ver
+	 * @param bool|false $inFooter
+	 * @param string     $hook
+	 *
+	 * @author Panagiotis Vagenas <pan.vagenas@gmail.com>
+	 * @since  TODO ${VERSION}
+	 */
+	public function enqueueScript(
+		$handle,
+		$src = false,
+		$deps = array(),
+		$ver = false,
+		$inFooter = false,
+		$hook = 'wp_enqueue_scripts'
+	) {
+		if ( ! wp_script_is( $handle, 'enqueued' ) ) {
+			if ( ! $ver ) {
+				$ver = $this->Plugin->getVersion();
+			}
+			add_action( $hook, function () use ( $handle, $src, $deps, $ver, $inFooter ) {
+				wp_enqueue_script( $handle, $src, $deps, $ver, $inFooter );
+			}, 20 );
+		}
+	}
+
+	/**
+	 * @param            $handle
+	 * @param bool|false $src
 	 * @param array      $deps
 	 * @param bool|false $ver
 	 * @param string     $media
@@ -61,16 +119,20 @@ class Scripts extends Core{
 	 * @author Panagiotis Vagenas <pan.vagenas@gmail.com>
 	 * @since  TODO ${VERSION}
 	 */
-	public function registerStyle($handle, $src, $deps = array(), $ver = false, $media = 'all'){
-		if(!$ver){
-			$ver = $this->Plugin->getVersion();
-		}
-		wp_register_style($handle, $src, $deps, $ver, $media);
+	public
+	function enqueueAdminStyle(
+		$handle,
+		$src = false,
+		$deps = array(),
+		$ver = false,
+		$media = 'all'
+	) {
+		$this->enqueueStyle( $handle, $src, $deps, $ver, $media, 'admin_enqueue_scripts' );
 	}
 
 	/**
 	 * @param            $handle
-	 * @param            $src
+	 * @param bool|false $src
 	 * @param array      $deps
 	 * @param bool|false $ver
 	 * @param bool|false $inFooter
@@ -78,35 +140,57 @@ class Scripts extends Core{
 	 * @author Panagiotis Vagenas <pan.vagenas@gmail.com>
 	 * @since  TODO ${VERSION}
 	 */
-	public function registerScript($handle, $src, $deps = array(), $ver = false, $inFooter = false){
-		if(!$ver){
-			$ver = $this->Plugin->getVersion();
-		}
-		wp_register_script($handle, $src, $deps, $ver, $inFooter);
+	public
+	function enqueueAdminScript(
+		$handle,
+		$src = false,
+		$deps = array(),
+		$ver = false,
+		$inFooter = false
+	) {
+		$this->enqueueScript( $handle, $src, $deps, $ver, $inFooter, 'admin_enqueue_scripts' );
 	}
 
 	/**
-	 * @param $handle
+	 * @param            $handle
+	 * @param bool|false $src
+	 * @param array      $deps
+	 * @param bool|false $ver
+	 * @param string     $media
 	 *
 	 * @author Panagiotis Vagenas <pan.vagenas@gmail.com>
 	 * @since  TODO ${VERSION}
 	 */
-	public function enqueueStyle($handle){
-		if(!wp_style_is($handle, 'enqueued') && wp_style_is($handle, 'registered')){
-			wp_enqueue_style($handle);
-		}
+	public
+	function enqueueLoginStyle(
+		$handle,
+		$src = false,
+		$deps = array(),
+		$ver = false,
+		$media = 'all'
+	) {
+		$this->enqueueStyle( $handle, $src, $deps, $ver, $media, 'login_enqueue_scripts' );
 	}
 
 	/**
-	 * @param $handle
+	 * @param            $handle
+	 * @param bool|false $src
+	 * @param array      $deps
+	 * @param bool|false $ver
+	 * @param bool|false $inFooter
 	 *
 	 * @author Panagiotis Vagenas <pan.vagenas@gmail.com>
 	 * @since  TODO ${VERSION}
 	 */
-	public function enqueueScript($handle){
-		if(!wp_script_is($handle, 'enqueued') && wp_script_is($handle, 'registered')){
-			wp_enqueue_script($handle);
-		}
+	public
+	function enqueueLoginScript(
+		$handle,
+		$src = false,
+		$deps = array(),
+		$ver = false,
+		$inFooter = false
+	) {
+		$this->enqueueScript( $handle, $src, $deps, $ver, $inFooter, 'login_enqueue_scripts' );
 	}
 
 	/**
@@ -116,7 +200,10 @@ class Scripts extends Core{
 	 * @author Panagiotis Vagenas <pan.vagenas@gmail.com>
 	 * @since  TODO ${VERSION}
 	 */
-	public function getScriptHandle($baseHandle){
+	public
+	function getScriptHandle(
+		$baseHandle
+	) {
 		return $this->Plugin->getSlug() . $baseHandle;
 	}
 
@@ -127,7 +214,10 @@ class Scripts extends Core{
 	 * @author Panagiotis Vagenas <pan.vagenas@gmail.com>
 	 * @since  TODO ${VERSION}
 	 */
-	public function getUrl($fileAbsPath){
-		return get_home_url() . '/' . str_replace(ABSPATH, '', $fileAbsPath);
+	public
+	function getUrl(
+		$fileAbsPath
+	) {
+		return get_home_url() . '/' . str_replace( ABSPATH, '', $fileAbsPath );
 	}
 }
