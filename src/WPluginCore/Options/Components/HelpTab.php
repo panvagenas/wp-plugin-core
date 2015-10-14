bd5d9df1f56ea1d91ebab73612203011ef38fd6c
Panagiotis Vagenas <pan.vagenas@gmail.com>
2015-10-14 15:53:39 +0300

    Implementing HelpTabs

--- src/WPluginCore/Options/Components/HelpTab.php
+++ src/WPluginCore/Options/Components/HelpTab.php
@@ -13,8 +13,21 @@ namespace WPluginCore002\Options\Components;
 
 
 use WPluginCore002\Abs\AbsClass;
+use WPluginCore002\Helpers\Random;
+use WPluginCore002\Plugin\Plugin;
 
-class HelpTab extends AbsClass{
+class HelpTab extends AbsClass {
+	protected $id;
+	protected $title;
+	protected $content;
+
+	public function __construct( Plugin $plugin, $title, $content, $id = '' ) {
+		parent::__construct( $plugin );
+		$this->title   = $title;
+		$this->content = $content;
+		$this->id      = $id ? $id : Random::lowStrengthRandomString( 10 );
+	}
+
 	/**
 	 * @return array
 	 * @author Panagiotis Vagenas <pan.vagenas@gmail.com>
@@ -31,5 +44,49 @@ class HelpTab extends AbsClass{
 		}
 
 		return $out;
+	}
+
+	/**
+	 * @return mixed
+	 * @author Panagiotis Vagenas <pan.vagenas@gmail.com>
+	 * @since  TODO ${VERSION}
+	 */
+	public function getTitle() {
+		return $this->title;
+	}
+
+	/**
+	 * @param mixed $title
+	 *
+	 * @return $this
+	 * @author Panagiotis Vagenas <pan.vagenas@gmail.com>
+	 * @since  TODO ${VERSION}
+	 */
+	public function setTitle( $title ) {
+		$this->title = $title;
+
+		return $this;
+	}
+
+	/**
+	 * @return mixed
+	 * @author Panagiotis Vagenas <pan.vagenas@gmail.com>
+	 * @since  TODO ${VERSION}
+	 */
+	public function getContent() {
+		return $this->content;
+	}
+
+	/**
+	 * @param mixed $content
+	 *
+	 * @return $this
+	 * @author Panagiotis Vagenas <pan.vagenas@gmail.com>
+	 * @since  TODO ${VERSION}
+	 */
+	public function setContent( $content ) {
+		$this->content = $content;
+
+		return $this;
 	}
 }
