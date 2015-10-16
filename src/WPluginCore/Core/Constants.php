47b5d3e1f5c2855610f7ea1899c1a81fa33d1554
Panagiotis Vagenas <pan.vagenas@gmail.com>
2015-10-16 09:10:39 +0300

    New objs

---
+++ src/WPluginCore/Core/Constants.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Project: wp-plugins-core.dev
+ * File: Constants.php
+ * User: Panagiotis Vagenas <pan.vagenas@gmail.com>
+ * Date: 15/10/2015
+ * Time: 10:00 μμ
+ * Since: TODO ${VERSION}
+ * Copyright: 2015 Panagiotis Vagenas
+ */
+
+namespace WPluginCore002\Core;
+
+
+class Constants {
+	const TYPE_BOOLEAN = 'bool';
+	const TYPE_STRING = 'string';
+	const TYPE_INTEGER = 'int';
+	const TYPE_FLOAT = 'float';
+	const TYPE_ARRAY = 'array';
+	const TYPE_OBJECT = 'object';
+	const TYPE_NULL = 'null';
+}
---
+++ src/WPluginCore/Helpers/String.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Project: wp-plugins-core.dev
+ * File: String.php
+ * User: Panagiotis Vagenas <pan.vagenas@gmail.com>
+ * Date: 15/10/2015
+ * Time: 9:59 μμ
+ * Since: TODO ${VERSION}
+ * Copyright: 2015 Panagiotis Vagenas
+ */
+
+namespace WPluginCore002\Helpers;
+
+
+class String {
+
+}
