4af5dd8b82c8e444b9aef08d7496c15bd43ba220
Panagiotis Vagenas <pan.vagenas@gmail.com>
2015-10-16 09:09:12 +0300

    Basic shortcode implementation

--- src/WPluginCore/Plugin/ShortCode.php
+++ src/WPluginCore/Plugin/ShortCode.php
@@ -13,7 +13,90 @@ namespace WPluginCore002\Plugin;
 
 
 use WPluginCore002\Abs\AbsClass;
+use WPluginCore002\Diagnostics\Exception;
 
+/**
+ * Class ShortCode
+ *
+ * @package WPluginCore002\Plugin
+ * @author  Panagiotis Vagenas <pan.vagenas@gmail.com>
+ * @since   TODO ${VERSION}
+ */
 class ShortCode extends AbsClass {
+	/**
+	 * Assoc array `['attr_name' => 'attr_default_value']`
+	 *
+	 * @var array
+	 */
+	protected $atts = array();
+	/**
+	 * Assoc array `['attr_name' => 'attr_type']`.
+	 * For attribute type you should use one of `Core\Constants::TYPE_*`
+	 *
+	 * @var array
+	 */
+	protected $types = array();
+	/**
+	 * @var bool
+	 */
+	protected $enclosing = false;
+	/**
+	 * @var string
+	 */
+	protected $tag;
 
+	/**
+	 * @param Plugin     $plugin
+	 * @param string     $tag
+	 * @param array      $atts
+	 * @param array      $types
+	 * @param bool|false $enclosing
+	 *
+	 * @throws Exception
+	 */
+	public function __construct( Plugin $plugin, $tag, $atts = array(), $types = array(), $enclosing = false ) {
+		parent::__construct( $plugin );
+
+		$this->tag       = (string) $tag;
+		$this->atts      = (array) $atts;
+		$this->types     = (array) $types;
+		$this->enclosing = (bool) $enclosing;
+
+		if ( array_diff_key( $atts, $types ) ) {
+			throw new Exception( 'Doing it wrong. Array keys don\'t match' );
+		}
+
+		add_shortcode( $this->tag, array( $this, 'prepare' ) );
+	}
+
+	/**
+	 * @param      $atts
+	 * @param null $content
+	 *
+	 * @return string
+	 * @author Panagiotis Vagenas <pan.vagenas@gmail.com>
+	 * @since  TODO ${VERSION}
+	 */
+	public function prepare( $atts, $content = null ) {
+		$atts = array_intersect_key( $atts, $this->atts );
+		$atts = shortcode_atts( $this->atts, $atts );
+
+		foreach ( $this->types as $attrName => $attrType ) {
+			settype( $atts[ $attrName ], $attrType );
+		}
+
+		return $this->output( $atts, $content );
+	}
+
+	/**
+	 * @param      $atts
+	 * @param null $content
+	 *
+	 * @return string
+	 * @author Panagiotis Vagenas <pan.vagenas@gmail.com>
+	 * @since  TODO ${VERSION}
+	 */
+	protected function output( $atts, $content = null ) {
+		return '';
+	}
 }
